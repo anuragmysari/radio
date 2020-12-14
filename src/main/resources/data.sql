f662c8dd1934c0056695fed1976c7aa9403f748c
anuragmysari <anuragmysari@gmail.com>
2020-12-14 00:38:07 -0500

    creating Indexes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ INSERT INTO Station VALUES( 'KFIAM40', 'More Simulating Talk',0,'KFI');
 INSERT INTO Station VALUES( 'Z100', 'Hit Music',1,'WHTZ');
 INSERT INTO Station VALUES( 'Z1002', 'Hit Music',1,'WHTZ2');
 INSERT INTO Station VALUES ('KOST103', 'KOST 103.5', '0', 'KOST');
+
+CREATE INDEX IDXID ON Station(stationId);
+CREATE INDEX IDXNAME ON Station(name);
